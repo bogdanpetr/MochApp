c44f62941c9212f5d0810405c822f03ab842490f
bogdanpetr <bogdanpetr@users.noreply.github.com>
2024-08-02 10:27:22 +0300

    Prepeare for the Login functionality.

--- app/src/main/java/com/app/mochapp/SecondActivity.kt
+++ app/src/main/java/com/app/mochapp/SecondActivity.kt
@@ -1,37 +1,20 @@
 package com.app.mochapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        clickOnButton(findViewById<Button>(R.id.btn_press)) {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-//            finish()
-        }
     }
-
-    private fun clickOnButton(button: Button, lambda: (() -> Unit)) {
-        button.setOnClickListener {
-            lambda.invoke()
-        }
-    }
-
 }
